83fdf2a5882fb7a0d980411df26f492a0602a34f
cx <597800451@qq.com>
2019-09-25 01:45:03 +0800

    添加样式类机制

--- components/RightComponents/RightComponents.vue
+++ components/RightComponents/RightComponents.vue
@@ -8,69 +8,14 @@
                                 <el-input :value="currentCompentenName" disabled size="mini"></el-input>
                             </el-form-item>
                             <el-divider><span style="color: #DCDFE6">属性</span></el-divider>
-                            <div style="display: flex;flex-wrap: wrap" class="form-line">
-                                <template v-for="(item,index) in formList">
-                                    <el-form-item v-show="haveMustCheck(item)" :label="item.label" class="i-form-item" :key="index">
-                                        <el-input v-if="item.type !== 'color' && item.type !== 'select' && item.type !== 'border'"
-                                                  v-model="iStyle[item.key]"
-                                                  :type="item.type"
-                                                  size="mini"
-                                                  @input="item.haveDirection === true ? paddingChange($event,index) : null"
-                                                  @focus="item.haveDirection === true ? paddingMarginDirectionFun(true,index) : null"
-                                        >
-                                        </el-input>
-                                        <div v-if="item.type === 'color'" style="display: inline-block;width: 120px;text-align: left;">
-                                            <el-color-picker v-model="iStyle[item.key]" :show-alpha="true"></el-color-picker>
-                                        </div>
-
-                                        <template v-if="item.type === 'border'">
-                                            <border :ikey="item.key" v-model="item.data" @choose="borderListShow = true"></border>
-                                        </template>
-
-                                        <el-select v-if="item.type === 'select'"
-                                                   v-model="iStyle[item.key]"
-                                                   placeholder="请选择"
-                                                   clearable
-                                        >
-                                            <el-option
-                                                    size="mini"
-                                                    v-for="item2 in item.select"
-                                                    :key="item2.value"
-                                                    :label="item2.label"
-                                                    :value="item2.value">
-                                            </el-option>
-                                        </el-select>
-                                    </el-form-item>
-
-                                    <template v-if="item.type === 'border'">
-                                        <div style="display: flex; flex-wrap: wrap" class="form-line" v-show="borderListShow">
-                                            <el-form-item :label="item2.label" class="i-form-item" v-for="item2 in borderList">
-                                                <border :ikey="item2.key" v-model="item2.data" ></border>
-                                            </el-form-item>
-                                        </div>
-                                        <el-divider v-if="borderListShow"><div style="cursor: pointer;" @click="borderListShow=false" ><span style="font-size: 14px;color: #ccc">收起</span><i class="el-icon-arrow-up" style="color: #ccc" ></i></div></el-divider>
-                                    </template>
-
-                                    <template v-if="item.haveDirection === true">
-                                        <div style="display: flex; flex-wrap: wrap" class="form-line" v-show="item.showDirection">
-                                            <el-form-item :label="item.key + item2.label" class="i-form-item" v-for="item2 in arrIndex">
-                                                    <el-input v-model="item.padingOrMargin[item2.name]" @input="directionFun(item2.name,$event,index)"  size="mini">
-                                                    </el-input>
-                                            </el-form-item>
-                                        </div>
-                                        <el-divider v-if="item.showDirection"><div style="cursor: pointer;" @click="paddingMarginDirectionFun(false,index)" ><span style="font-size: 14px;color: #ccc">收起</span><i class="el-icon-arrow-up" style="color: #ccc" ></i></div></el-divider>
-                                    </template>
-                                </template>
-                            </div>
-                            <el-form-item label="样式配置" style="margin: 15px 0">
-                                <el-input class="style-json" type="textarea" v-model="iStyleJson" :autosize="{ minRows:6 }"></el-input>
-                            </el-form-item>
+                            <style-panel v-show="currentCompentenName !== ''" ></style-panel>
                         </el-form>
                 </el-tab-pane>
                 <el-tab-pane label="自定属性" name="second">
                     <right-com-style-add></right-com-style-add>
                 </el-tab-pane>
                 <el-tab-pane label="制定类" name="three">
+                    <right-com-class-add></right-com-class-add>
                 </el-tab-pane>
                 <el-tab-pane label="目录" name="four">
                 </el-tab-pane>
@@ -80,170 +25,42 @@
 </template>
 
 <script>
-    import border from './styleCompentens/border'
     import {dIndex,arrIndex,borderList} from '@/common/js/styleExternalData'
     import rightComStyleAdd from './rightComStyleAdd'
+    import stylePanel from './styleCompentens/stylePanel'
+    import rightComClassAdd from './rightComClassAdd'
 
     export default {
         name: 'RightComponents',
         data(){
             return {
                 activeName: 'first',
-                paddingMarginDirection: false,
-                checkMOrP: '',
-                arrIndex:arrIndex,
-                borderList:borderList,
-                borderListShow: false
             }
         },
         watch:{
         },
         methods:{
-            onmousewheel(){
-                console.log(11)
-            },
-            /**
-             * 校验样式依赖关系
-             * 控制是否显示
-             */
-            haveMustCheck(item){
-                let {must} = item
-                if (must){
-                    let {key,value} = must
-                    let parent = this.iStyle[key]
-                    if ( parent && parent===value ){
-                        return true
-                    }
-                    return false
-                }else {
-                    return true
-                }
-            },
-            arr2four(index,arr) {
-                let newArr = arr
-                if (index === 2){
-                     newArr = [arr[0],arr[1],arr[0],arr[1]]
-                }
-                if (index === 1){
-                     newArr = [arr[0],arr[0],arr[0],arr[0]]
-                }
-                if (index === 0){
-                    newArr = [0,0,0,0]
-                }
-                return newArr
-            },
             handleClick(){
 
             },
-            directionFun(d,val,index){
-                let mp = this.formList[index].key
-                let style = this.iStyle
-                let arrStyle = []
-                if (style[mp]){
-                    arrStyle = style[mp].split(" ")
-                }
-                arrStyle = this.arr2four(arrStyle.length,arrStyle)
-                arrStyle[dIndex[d]] = val
-                style[mp] = arrStyle.join(" ")
-
-                this.$store.commit('setCurrentCheckItemStyle',style)
-            },
-            paddingMarginDirectionFun(flag,index){
-                this.checkMOrP = this.formList[index].key
-                this.formList[index].showDirection = flag
-                this.computedMarginOrPadding(this.iStyle[this.formList[index].key],index)
-            },
-            paddingChange(val,index){
-                this.computedMarginOrPadding(val,index)
-            },
-            computedMarginOrPadding(val,index){
-
-                if (!val || val === ''){
-                    this.formList[index].padingOrMargin.top = ''
-                    this.formList[index].padingOrMargin.buttom = ''
-                    this.formList[index].padingOrMargin.left = ''
-                    this.formList[index].padingOrMargin.right = ''
-                    return
-                }
-                val = val.toString().trim()
-                let arrVal = val.split(" ")
-                if (arrVal.length === 1){
-                    this.formList[index].padingOrMargin.top = arrVal[0]
-                    this.formList[index].padingOrMargin.buttom = arrVal[0]
-                    this.formList[index].padingOrMargin.left = arrVal[0]
-                    this.formList[index].padingOrMargin.right = arrVal[0]
-                }else   if (arrVal.length === 2){
-                    this.formList[index].padingOrMargin.top = arrVal[0]
-                    this.formList[index].padingOrMargin.buttom = arrVal[0]
-                    this.formList[index].padingOrMargin.left = arrVal[1]
-                    this.formList[index].padingOrMargin.right = arrVal[1]
-                }else if (arrVal.length === 4){
-                    this.formList[index].padingOrMargin.top = arrVal[0]
-                    this.formList[index].padingOrMargin.buttom = arrVal[2]
-                    this.formList[index].padingOrMargin.left = arrVal[3]
-                    this.formList[index].padingOrMargin.right = arrVal[1]
-                }else {
-                    this.formList[index].padingOrMargin.top = ''
-                    this.formList[index].padingOrMargin.buttom = ''
-                    this.formList[index].padingOrMargin.left = ''
-                    this.formList[index].padingOrMargin.right = ''
-                }
-            }
         },
         computed:{
-            formList(){
-                return this.$store.state.currentCheckAttr.formList
-            },
-            list(){
-                return this.$store.state.list
-            },
-            iStyleJson: {
-                get(){
-                    if (this.$store.state.currentCheckAttr.item){
-                        return JSON.stringify(this.$store.state.currentCheckAttr.item.iStyle,null,'    ')
-                    }
-                    return ''
-                },
-                set(val){
-                    this.$store.commit('setCurrentCheckItemStyle',JSON.parse(val))
-                }
-            },
-            iStyle: {
-                get(){
-                    if (this.$store.state.currentCheckAttr.item){
-                        return this.$store.state.currentCheckAttr.item.iStyle
-                    }
-                    return {}
-                },
-                set(val){
-                    this.$store.commit('setCurrentCheckItemStyle',val)
-                }
-            },
             currentCompentenName(){
                 if (this.$store.state.currentCheckAttr.item){
                     return this.$store.state.currentCheckAttr.item.name
                 }
                 return ''
-            },
-            currentCompentenItem(){
-                if (this.$store.state.currentCheckAttr.item){
-                    return this.$store.state.currentCheckAttr.item
-                }
-                return {}
             }
         },
         components:{
-            border,
             rightComStyleAdd,
+            stylePanel,
+            rightComClassAdd
         }
     }
 </script>
 
 <style scoped lang="scss">
-    .i-form-item{
-        width: 50%;
-    }
-
     .text {
         font-size: 14px;
     }
@@ -254,11 +71,5 @@
 
     .el-form-item{
         margin-bottom: 5px;
-    }
-
-    .form-line{
-    }
-
-    .style-json{
     }
 </style>
---
+++ components/RightComponents/styleCompentens/stylePanel.vue
@@ -0,0 +1,210 @@
+<template>
+    <view>
+        <div style="display: flex;flex-wrap: wrap" class="form-line" >
+            <template v-for="(item,index) in formList">
+                <el-form-item v-show="haveMustCheck(item)" :label="item.label" class="i-form-item" :key="index">
+                    <el-input v-if="item.type !== 'color' && item.type !== 'select' && item.type !== 'border'"
+                              v-model="iStyle[item.key]"
+                              :type="item.type"
+                              size="mini"
+                              @input="item.haveDirection === true ? paddingChange($event,index) : null"
+                              @focus="item.haveDirection === true ? paddingMarginDirectionFun(true,index) : null"
+                    >
+                    </el-input>
+                    <div v-if="item.type === 'color'" style="display: inline-block;width: 120px;text-align: left;">
+                        <el-color-picker v-model="iStyle[item.key]" :show-alpha="true"></el-color-picker>
+                    </div>
+
+                    <template v-if="item.type === 'border'">
+                        <border :ikey="item.key" v-model="item.data" @choose="borderListShow = true"></border>
+                    </template>
+
+                    <el-select v-if="item.type === 'select'"
+                               v-model="iStyle[item.key]"
+                               placeholder="请选择"
+                               clearable
+                    >
+                        <el-option
+                                size="mini"
+                                v-for="item2 in item.select"
+                                :key="item2.value"
+                                :label="item2.label"
+                                :value="item2.value">
+                        </el-option>
+                    </el-select>
+                </el-form-item>
+
+                <template v-if="item.type === 'border'">
+                    <div style="display: flex; flex-wrap: wrap" class="form-line" v-show="borderListShow">
+                        <el-form-item :label="item2.label" class="i-form-item" v-for="item2 in borderList">
+                            <border :ikey="item2.key" v-model="item2.data" ></border>
+                        </el-form-item>
+                    </div>
+                    <el-divider v-if="borderListShow"><div style="cursor: pointer;" @click="borderListShow=false" ><span style="font-size: 14px;color: #ccc">收起</span><i class="el-icon-arrow-up" style="color: #ccc" ></i></div></el-divider>
+                </template>
+
+                <template v-if="item.haveDirection === true">
+                    <div style="display: flex; flex-wrap: wrap" class="form-line" v-show="item.showDirection">
+                        <el-form-item :label="item.key + item2.label" class="i-form-item" v-for="item2 in arrIndex">
+                            <el-input v-model="item.padingOrMargin[item2.name]" @input="directionFun(item2.name,$event,index)"  size="mini">
+                            </el-input>
+                        </el-form-item>
+                    </div>
+                    <el-divider v-if="item.showDirection"><div style="cursor: pointer;" @click="paddingMarginDirectionFun(false,index)" ><span style="font-size: 14px;color: #ccc">收起</span><i class="el-icon-arrow-up" style="color: #ccc" ></i></div></el-divider>
+                </template>
+            </template>
+        </div>
+        <el-form-item label="样式配置" style="margin: 15px 0">
+            <el-input class="style-json" type="textarea" v-model="iStyleJson" :autosize="{ minRows:6 }"></el-input>
+        </el-form-item>
+    </view>
+</template>
+
+<script>
+    import border from './border'
+    import {dIndex,arrIndex,borderList} from '@/common/js/styleExternalData'
+
+    export default {
+        name: 'stylePanel',
+        data(){
+            return {
+                paddingMarginDirection: false,
+                checkMOrP: '',
+                arrIndex:arrIndex,
+                borderList:borderList,
+                borderListShow: false
+            }
+        },
+        methods:{
+            /**
+             * 校验样式依赖关系
+             * 控制是否显示
+             */
+            haveMustCheck(item){
+                let {must} = item
+                if (must){
+                    let {key,value} = must
+                    let parent = this.iStyle[key]
+                    if ( parent && parent===value ){
+                        return true
+                    }
+                    return false
+                }else {
+                    return true
+                }
+            },
+            arr2four(index,arr) {
+                let newArr = arr
+                if (index === 2){
+                    newArr = [arr[0],arr[1],arr[0],arr[1]]
+                }
+                if (index === 1){
+                    newArr = [arr[0],arr[0],arr[0],arr[0]]
+                }
+                if (index === 0){
+                    newArr = [0,0,0,0]
+                }
+                return newArr
+            },
+            directionFun(d,val,index){
+                let mp = this.formList[index].key
+                let style = this.iStyle
+                let arrStyle = []
+                if (style[mp]){
+                    arrStyle = style[mp].split(" ")
+                }
+                arrStyle = this.arr2four(arrStyle.length,arrStyle)
+                arrStyle[dIndex[d]] = val
+                style[mp] = arrStyle.join(" ")
+
+                this.$store.commit('setCurrentCheckItemStyle',style)
+            },
+            paddingMarginDirectionFun(flag,index){
+                this.checkMOrP = this.formList[index].key
+                this.formList[index].showDirection = flag
+                this.computedMarginOrPadding(this.iStyle[this.formList[index].key],index)
+            },
+            paddingChange(val,index){
+                this.computedMarginOrPadding(val,index)
+            },
+            computedMarginOrPadding(val,index){
+
+                if (!val || val === ''){
+                    this.formList[index].padingOrMargin.top = ''
+                    this.formList[index].padingOrMargin.buttom = ''
+                    this.formList[index].padingOrMargin.left = ''
+                    this.formList[index].padingOrMargin.right = ''
+                    return
+                }
+                val = val.toString().trim()
+                let arrVal = val.split(" ")
+                if (arrVal.length === 1){
+                    this.formList[index].padingOrMargin.top = arrVal[0]
+                    this.formList[index].padingOrMargin.buttom = arrVal[0]
+                    this.formList[index].padingOrMargin.left = arrVal[0]
+                    this.formList[index].padingOrMargin.right = arrVal[0]
+                }else   if (arrVal.length === 2){
+                    this.formList[index].padingOrMargin.top = arrVal[0]
+                    this.formList[index].padingOrMargin.buttom = arrVal[0]
+                    this.formList[index].padingOrMargin.left = arrVal[1]
+                    this.formList[index].padingOrMargin.right = arrVal[1]
+                }else if (arrVal.length === 4){
+                    this.formList[index].padingOrMargin.top = arrVal[0]
+                    this.formList[index].padingOrMargin.buttom = arrVal[2]
+                    this.formList[index].padingOrMargin.left = arrVal[3]
+                    this.formList[index].padingOrMargin.right = arrVal[1]
+                }else {
+                    this.formList[index].padingOrMargin.top = ''
+                    this.formList[index].padingOrMargin.buttom = ''
+                    this.formList[index].padingOrMargin.left = ''
+                    this.formList[index].padingOrMargin.right = ''
+                }
+            }
+        },
+        computed:{
+            formList(){
+                return this.$store.state.currentCheckAttr.formList
+            },
+            list(){
+                return this.$store.state.list
+            },
+            iStyleJson: {
+                get(){
+                    if (this.$store.state.currentCheckAttr.item){
+                        return JSON.stringify(this.$store.state.currentCheckAttr.item.iStyle,null,'    ')
+                    }
+                    return ''
+                },
+                set(val){
+                    this.$store.commit('setCurrentCheckItemStyle',JSON.parse(val))
+                }
+            },
+            iStyle: {
+                get(){
+                    if (this.$store.state.currentCheckAttr.item){
+                        return this.$store.state.currentCheckAttr.item.iStyle
+                    }
+                    return {}
+                },
+                set(val){
+                    this.$store.commit('setCurrentCheckItemStyle',val)
+                }
+            },
+        },
+        components:{
+            border
+        }
+    }
+</script>
+
+<style scoped>
+    .i-form-item{
+        width: 50%;
+    }
+
+    .form-line{
+    }
+
+    .style-json{
+    }
+</style>
